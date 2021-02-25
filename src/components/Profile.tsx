092c25186c9db33a4f8bc0ec4b83a872a87e9b29
Pedro Lucas <pedronun@protonmail.com>
2021-02-24 21:12:23 -0300

    Added Profile, Experience Bar and Completed Challenges components

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/Plrns.png" alt="Pedro Lucas"/>
+      <div>
+        <strong>Pedro Lucas</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level Icon"/>
+          Level 1
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
